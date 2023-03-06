08107b8497aeb80016d0efe199434713fd158fc7
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-03-06 09:43:29 +0700

    add validator in order query

--- src/stock/dto/marketLiquidityQuery.dto.ts
+++ src/stock/dto/marketLiquidityQuery.dto.ts
@@ -1,9 +1,9 @@
 import {ApiProperty} from "@nestjs/swagger";
-import {IsNumberString} from "class-validator";
+import {IsEnum} from "class-validator";
 
 
 export class MarketLiquidityQueryDto {
-    @IsNumberString()
+    @IsEnum(["0","1","2","3","-1"], {message: 'order not found'})
     @ApiProperty({
         type: Number,
         example: 0,
