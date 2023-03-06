d1480615606391c82b1b6f93291c8414841dad86
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-03-06 15:55:06 +0700

    Update compress.sh

--- .github/workflows/compress.sh
+++ .github/workflows/compress.sh
@@ -4,7 +4,7 @@ SERVICE="SERVICE"
 DEST="./docker"
 
 # Compress the code into SERVICE.tgz
-tar -zcvf ${SERVICE}.tgz --exclude="node_modules" .
+tar -zcvf ${SERVICE}.tgz --exclude="node_modules" *
 
 # Move SERVICE.tgz into the ./docker folder
 mv ${SERVICE}.tgz ${DEST}/
