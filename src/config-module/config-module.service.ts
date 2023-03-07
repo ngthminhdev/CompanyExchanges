e89c17ff9e1ad2128d4b7f114b33cbbbec73e9ef
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-03-07 09:39:14 +0700

    change redis url

--- src/config-module/config-module.service.ts
+++ src/config-module/config-module.service.ts
@@ -38,7 +38,8 @@ export class ConfigServiceProvider {
   async createRedisOptions(): Promise<any> {
     return {
       store: await redisStore({
-        url: process.env.REDIS_URL,
+        // url: process.env.REDIS_URL,
+        url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/${process.env.REDIS_DB}`,
         ttl: TimeToLive.Minute
       }),
     };
--- src/enums/common.enum.ts
+++ src/enums/common.enum.ts
@@ -1,4 +1,4 @@
-export enum     BooleanEnum {
+export enum  BooleanEnum {
     False,
     True
 }
