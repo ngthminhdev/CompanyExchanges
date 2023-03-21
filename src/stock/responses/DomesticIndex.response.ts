cfb0b1f4a0f966f24fe88cd87ba012b74ff3eef6
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-03-21 10:18:15 +0700

    Refactor: Change field in response

--- src/stock/responses/DomesticIndex.response.ts
+++ src/stock/responses/DomesticIndex.response.ts
@@ -15,7 +15,7 @@ export class DomesticIndexResponse {
         type: Number,
         example: 1502.9
     })
-    close_price: number;
+    price: number;
 
     @ApiProperty({
         type: Number,
@@ -31,16 +31,15 @@ export class DomesticIndexResponse {
 
     @ApiProperty({
         type: Date,
-        example: 1502.9
     })
-    yyyymmdd: Date | string;
+    lastUpdated: Date | string;
 
     constructor(data?: DomesticIndexInterface) {
         this.ticker = data?.ticker || '';
-        this.close_price = data?.close_price || 0;
+        this.price = data?.close_price || 0;
         this.change_price = data?.change_price|| 0;
         this.percent_d = +data?.percent_d || 0;
-        this.yyyymmdd = UtilCommonTemplate.toDate(data?.date_time) || 0;
+        this.lastUpdated = UtilCommonTemplate.toDateTime(data?.date_time) || "";
     }
 
     public mapToList(data?: DomesticIndexInterface[] | any[]) {
