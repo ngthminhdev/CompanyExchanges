f9dfce337d063208b729d5536930688b5961083c
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-03-21 10:15:07 +0700

    Chore: Add topic and event listener

--- src/enums/kafka-topics.enum.ts
+++ src/enums/kafka-topics.enum.ts
@@ -1,11 +1,15 @@
 export enum Topics {
   DoRongThiTruong = 'kafka-do-rong-thi-truong-topic',
   ThanhKhoanPhienHienTai = 'kafka-thanh-khoan-phien-hien-tai-topic',
-  PhanNganh = 'kafka-phan-nganh-topic'
+  PhanNganh = 'kafka-phan-nganh-topic',
+  ChiSoTrongNuoc = 'kakfa-chi-so-trong-nuoc-topic',
+  BienDongThiTruong = 'kafka-bien-dong-thi-truong-topic'
 }
 
 export const requestPatterns: string[] = [
-    Topics.DoRongThiTruong,
-    Topics.ThanhKhoanPhienHienTai,
-    Topics.PhanNganh
+    // Topics.DoRongThiTruong,
+    // Topics.ThanhKhoanPhienHienTai,
+    // Topics.PhanNganh
+    Topics.ChiSoTrongNuoc,
+    Topics.BienDongThiTruong
 ];
