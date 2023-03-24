7f5c7caba392c465eda406229d05c468977e2513
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-03-24 15:02:29 +0700

    Fix: Remove secure in login api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -90,7 +90,7 @@ export class AuthService {
 
             res.cookie('refreshToken', refreshToken, {
                 path: '/',
-                secure: true,
+                secure: false,
                 sameSite: "none"
             });
 
