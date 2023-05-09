4bad076e4b2049228493cc0ebdca2dc75de567dd
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-05-09 09:50:58 +0700

    Fix: Fix industry child missing date data

--- src/stock/processes/industry-child.ts
+++ src/stock/processes/industry-child.ts
@@ -1,37 +1,51 @@
-import connectDB from "../../utils/utils.connect-database";
-import {ExceptionResponse} from "../../exceptions/common.exception";
-import {HttpStatus} from "@nestjs/common";
+import connectDB from '../../utils/utils.connect-database';
+import { ExceptionResponse } from '../../exceptions/common.exception';
+import { HttpStatus } from '@nestjs/common';
 
 process.on('message', async (data: any) => {
-    try {
-        const { marketCapQuery } = data;
-        
-        // tạo database connection mới và thực hiện truy vấn
-        const sql = await connectDB();
+  try {
+    const { marketCapQuery } = data;
 
-        const marketCap = (await sql.query(marketCapQuery)).recordset;
+    // tạo database connection mới và thực hiện truy vấn
+    const sql = await connectDB();
 
-        const groupByIndustry = marketCap.reduce((result, item) => {
-            (result[item.industry] || (result[item.industry] = [])).push(item);
-            return result;
-        }, {});
+    const marketCap = (await sql.query(marketCapQuery)).recordset;
 
-        //Calculate change percent per day, week, month
-        const industryChanges = Object.entries(groupByIndustry).map(([industry, values]: any) => {
-            return {
-                industry,
-                day_change_percent: ((values[0].total_market_cap - values[1].total_market_cap) / values[1].total_market_cap) * 100,
-                week_change_percent: ((values[0].total_market_cap - values[1].total_market_cap) / values[1].total_market_cap) * 100,
-                month_change_percent: ((values[0].total_market_cap - values[2].total_market_cap) / values[2].total_market_cap) * 100,
-                ytd: ((values[0].total_market_cap - values[3].total_market_cap) / values[3].total_market_cap) * 100,
-            };
-        });
+    const groupByIndustry = marketCap.reduce((result, item) => {
+      (result[item.industry] || (result[item.industry] = [])).push(item);
+      return result;
+    }, {});
 
-        // gửi kết quả truy vấn về cho process cha
-        process.send(industryChanges);
-        await sql.close();
-    } catch (e) {
-        console.log(e)
-        throw new ExceptionResponse(HttpStatus.BAD_REQUEST, 'Error');
-    }
+    //Calculate change percent per day, week, month
+    const industryChanges = Object.entries(groupByIndustry).map(
+      ([industry, values]: any) => {
+        return {
+          industry,
+          day_change_percent:
+            ((values[0].total_market_cap - values[1].total_market_cap) /
+              values[1].total_market_cap) *
+            100,
+          week_change_percent:
+            ((values[0].total_market_cap - values[2].total_market_cap) /
+              values[2].total_market_cap) *
+            100,
+          month_change_percent:
+            ((values[0].total_market_cap - values[3].total_market_cap) /
+              values[3].total_market_cap) *
+            100,
+          ytd:
+            ((values[0].total_market_cap - values[4].total_market_cap) /
+              values[4].total_market_cap) *
+            100,
+        };
+      },
+    );
+
+    // gửi kết quả truy vấn về cho process cha
+    process.send(industryChanges);
+    await sql.close();
+  } catch (e) {
+    console.log(e);
+    throw new ExceptionResponse(HttpStatus.BAD_REQUEST, 'Error');
+  }
 });
