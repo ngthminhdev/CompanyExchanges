88bb07fd4fbbcba21a268f8341e6168181c9992c
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-05-18 11:00:39 +0700

    Chore: remove swagger logs

--- src/stock/dto/cashTypeQuery.dto.ts
+++ src/stock/dto/cashTypeQuery.dto.ts
@@ -3,13 +3,13 @@ import { IsEnum } from 'class-validator';
 import { TimestampQueryOnlyDto } from './timestampOnlyQuery.dto';
 
 export class CashTypeQueryDto extends TimestampQueryOnlyDto {
+  @IsEnum(['0', '1', '2'], { message: 'investorType not found' })
   @ApiProperty({
     type: Number,
     example: 0,
     description: `<p>
             <div><font color="#228b22">0: </font><font color="gray">Khoi ngoai</font></div>
             <div><font color="#228b22">1: </font><font color="gray">Tu doanh</font></div>
-            <div><font color="#228b22">2: </font><font color="gray">Ca nhan</font></div>
             <div><font color="#228b22">2: </font><font color="gray">Ca nhan</font></div>
         </p>`,
   })
