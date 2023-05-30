100a2a58a9ddcc9109bd24f792de4fc8a05b1341
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-05-30 11:41:46 +0700

    Fix: fix date format

--- src/kafka/responses/LineChart.response.ts
+++ src/kafka/responses/LineChart.response.ts
@@ -1,31 +1,34 @@
-import { ApiProperty, ApiResponseProperty, PartialType } from "@nestjs/swagger";
-import { VnIndexResponse } from "../../stock/responses/Vnindex.response";
-import { BaseResponse } from "../../utils/utils.response";
-import { LineChartInterface } from "../interfaces/line-chart.interface";
+import { ApiProperty, ApiResponseProperty, PartialType } from '@nestjs/swagger';
+import { VnIndexResponse } from '../../stock/responses/Vnindex.response';
+import { BaseResponse } from '../../utils/utils.response';
+import { LineChartInterface } from '../interfaces/line-chart.interface';
 
+export class LineChartResponse extends VnIndexResponse {
+  @ApiResponseProperty({
+    type: Number,
+  })
+  tradingDate: number;
 
-export class LineChartResponse extends VnIndexResponse{
-    @ApiResponseProperty({
-        type: Number
-    })
-    tradingDate: number;
+  constructor(data?: LineChartInterface) {
+    super(data);
+    this.tradingDate = Date.UTC(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      new Date().getDate(),
+      new Date(data?.tradingDate)?.getHours(),
+      new Date(data?.tradingDate)?.getMinutes(),
+    ).valueOf();
+  }
 
-    constructor(data?: LineChartInterface) {
-        super(data);
-        this.tradingDate = Date.UTC(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 
-        new Date(data?.tradingDate)?.getHours(), 
-        new Date(data?.tradingDate)?.getMinutes()).valueOf()
-    }
-
-    public mapToList(data?: LineChartInterface[]) {
-        return data.map(i => new LineChartResponse(i))
-    }
+  public mapToList(data?: LineChartInterface[]) {
+    return data.map((i) => new LineChartResponse(i));
+  }
 }
 
 export class LineChartSwagger extends PartialType(BaseResponse) {
-    @ApiProperty({
-        type: LineChartResponse,
-        isArray: true,
-    })
-    data: LineChartResponse[];
+  @ApiProperty({
+    type: LineChartResponse,
+    isArray: true,
+  })
+  data: LineChartResponse[];
 }
