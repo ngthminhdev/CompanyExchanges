4434f2a6d769c691cc5b41cc761ae45e145501cf
Chau Minh <minhnlc@aloapp.vn>
2023-07-17 10:42:05 +0700

    feat: thêm 2 api site fdi, sửa lại api map xuất nhập khẩu

--- src/macro/dto/foreign-investment-index.dto.ts
+++ src/macro/dto/foreign-investment-index.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum } from "class-validator";
 import { FDIOrderDto } from "./fdi-order.dto";
 
 export class ForeignInvestmentIndexDto extends FDIOrderDto {
+    @IsEnum(['1', '2'], {message: 'type invalid'})
     @ApiProperty({
         type: Number,
         description: `0 - Đối tác, 1 - Ngành`
