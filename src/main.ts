32b7e2a6f1ee5664b41aca221ba3bae199f6e5d1
Chau Minh <minhnlc@aloapp.vn>
2023-10-23 15:52:00 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   //   credentials: true,
   //   allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
   // });
-  app.enableCors({ origin: 'http://192.168.12.243:3000', credentials: true});
+  app.enableCors({ origin: 'http://192.168.12.243:3000', credentials: true, allowedHeaders: 'Access-Control-Allow-Credentials'});
   app.use(cookieParser());
   app.setGlobalPrefix(process.env.API_PREFIX);
   app.useGlobalInterceptors(new HttpLoggerInterceptor());
