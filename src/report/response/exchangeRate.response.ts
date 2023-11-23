9d3977e5ada0380044f05e2c8bc3c3c238a50e2a
Chau Minh <minhnlc@aloapp.vn>
2023-11-23 15:09:03 +0700

    feat: thêm hình quốc kì

--- src/report/response/exchangeRate.response.ts
+++ src/report/response/exchangeRate.response.ts
@@ -30,12 +30,19 @@ export class ExchangeRateResponse {
     })
     year: number
 
+    @ApiProperty({
+        type: String,
+        description: 'Hình quốc kì'
+    })
+    img: string
+
     constructor(data?: ExchangeRateResponse) {
         this.code = data?.code || ''
         this.price = data?.price || 0
         this.day = +data?.day.toFixed(2) || 0
         this.month = +data?.month.toFixed(2) || 0
         this.year = +data?.year.toFixed(2) || 0
+        this.img = `/resources/national/${data.code}.png`
     }
 
     static mapToList(data?: ExchangeRateResponse[]) {
