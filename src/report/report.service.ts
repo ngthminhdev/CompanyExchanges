438bac03e82d79409a5e69a7777a6e5243b95504
Chau Minh <minhnlc@aloapp.vn>
2023-11-24 15:22:09 +0700

    fix: đổi jpg sang png

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -512,7 +512,7 @@ export class ReportService {
       const [data_1, data_2] = await Promise.all([promise_1, promise_2]) as any
       const floor: any = await this.mssqlService.query(`select code, floor from marketInfor.dbo.info where code in (${data_2.map(item => `'${item.code}'`).join(`,`)})`)
       
-      return {text: [data_1[0].text_1, data_1[0].text_2], stock: data_2.map(item => ({...item, img: `/resources/stock/${item.code}_${(floor.find(fl => fl.code == item.code)).floor}.jpg`}))}
+      return {text: [data_1[0].text_1, data_1[0].text_2], stock: data_2.map(item => ({...item, img: `/resources/stock/${item.code}_${(floor.find(fl => fl.code == item.code)).floor}.png`}))}
     } catch (e) {
       throw new CatchException(e)
     }
