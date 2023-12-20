d8ffeddc9ea73539bb6dda600a18a4c4fa1dbd98
Chau Minh <minhnlc@aloapp.vn>
2023-12-20 10:43:24 +0700

    feat: thêm prevClosePrice vẽ chart

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -761,7 +761,7 @@ export class ReportService {
       //thong tin vnindex
       const promise_2 = this.mssqlService.query(`
       with temp as (
-        select i.date, f.netVal, closePrice, change, perChange, totalVal, (totalVal - lead(totalVal) over (partition by i.code order by i.date desc)) / lead(totalVal) over (partition by i.code order by i.date desc) * 100 as perTotalVal, advances, declines, noChange, ceilingStocks, floorStocks, highPrice, lowPrice from marketTrade.dbo.indexTradeVND i
+        select i.date, f.netVal, closePrice, lead(closePrice) over (partition by i.code order by i.date desc) as prevClosePrice, perChange, totalVal, (totalVal - lead(totalVal) over (partition by i.code order by i.date desc)) / lead(totalVal) over (partition by i.code order by i.date desc) * 100 as perTotalVal, advances, declines, noChange, ceilingStocks, floorStocks, highPrice, lowPrice from marketTrade.dbo.indexTradeVND i
         inner join marketTrade.dbo.[foreign] f on f.code = i.code and f.date = i.date
          where i.code = 'VNINDEX')
               select top 1 * from temp
@@ -837,6 +837,7 @@ export class ReportService {
       return new AfternoonReport1({
         text: data_8 || [],
         closePrice: data_2[0].closePrice,
+        prevClosePrice: data_2[0].prevClosePrice,
         change: data_2[0].change,
         perChange: data_2[0].perChange,
         totalVal: data_2[0].totalVal,
